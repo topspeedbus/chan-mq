e5f208cf8f964e6d023490e396b1bb4b470c8a49
chenjie23333 <243987701@qq.com>
2020-05-27 10:51:03 +0800

    添加消息持久化

--- src/main/java/com/chan/mq/controller/SendMsgController.java
+++ src/main/java/com/chan/mq/controller/SendMsgController.java
@@ -1,5 +1,6 @@
 package com.chan.mq.controller;
 
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -29,14 +30,27 @@ public class SendMsgController {
 
     @GetMapping("v1")
     public void test1(@RequestParam(value = "message") String message) {
+
+        /**
+         * cap:consistency, availability, partition tolerance
+         * 为什么要放弃P： p就相当于基础服务的运行情况，如果不保证P 意味着p随时崩掉
+         * 如果没有基础服务，那c a 也无法存在
+         *
+         *
+         * base: basically available soft state eventual consistency
+         */
         MessagePostProcessor messagePostProcessor = msg -> {
             MessageProperties messageProperties = msg.getMessageProperties();
             // 设置编码
             messageProperties.setContentEncoding("utf-8");
             // 设置过期时间10*1000毫秒
             messageProperties.setExpiration("5000");
+            //消息定位：实现幂等
+            messageProperties.setCorrelationId("");
+            messageProperties.getCorrelationId();
+            //持久化三点：交换机，队列，消息。调用：fsync指令非实时，消息无法完全做到不流失。设置消息持久化：注意影响性能
+            messageProperties.setDeliveryMode(MessageDeliveryMode.PERSISTENT);
             return msg;
-        };
-        rabbitTemplate.convertAndSend("test_dead_ex", "test_dead_routing_key", message + "------>" + new Date(), messagePostProcessor);
+        };        rabbitTemplate.convertAndSend("test_dead_ex", "test_dead_routing_key", message + "------>" + new Date(), messagePostProcessor);
     }
 }
